1af24dcf7aa4aff495bddfcad83907f5d0802fe3
om2137 <omraut961@gmail.com>
2025-11-19 11:34:18 +0530

    timeout component for p1 and p2

---
+++ components/Timeout.tsx
@@ -0,0 +1,168 @@
+import { getSigner } from '@/utils/connect'
+import { ethers } from 'ethers';
+import RPSArtificate from '@/utils/RPS.json'
+import { useEffect, useState } from 'react';
+import Button from './Button';
+
+
+export function J1Timeout({ gameAddress }: { gameAddress: string }) {
+
+    const [timeoutData, setTimeoutData] = useState<{
+        lastAction: number;
+        Timeout: number;
+        c2: number;
+    }>();
+    const [timer, setTimer] = useState<string>();
+    const [p2Played, setP2Played] = useState(false);
+    // const [remaining, setRemaining] = useState(0);
+
+    async function Loadtimeout() {
+        try {
+            const signer = await getSigner();
+            const game = new ethers.Contract(gameAddress, RPSArtificate.abi, signer);
+            const lastaction = Number(await game.lastAction());
+            const timeout = Number(await game.TIMEOUT());
+            const c2value = Number(await game.c2());
+
+            setTimeoutData({
+                lastAction: lastaction,
+                Timeout: timeout,
+                c2: c2value
+            })
+
+            const t = new Date((lastaction + timeout) * 1000)
+                .toLocaleTimeString("en-GB", { hour12: false });
+            setTimer(t)
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    async function callTimeout() {
+        try {
+            const signer = await getSigner();
+            const contract = new ethers.Contract(gameAddress, RPSArtificate.abi, signer);
+            const j2Timeout = await contract.j2Timeout();
+            await j2Timeout.wait();
+            alert(` executed successfully!`);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    async function watcher() {
+        const signer = await getSigner();
+        const game = new ethers.Contract(gameAddress, RPSArtificate.abi, signer);
+        const c2 = await game.c2();
+
+        if (Number(c2) !== 0) setP2Played(true);
+    }
+
+    useEffect(() => {
+        if (gameAddress) Loadtimeout();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [gameAddress]);
+
+    useEffect(() => {
+        if (!gameAddress) return;
+
+        const interval = setInterval(async () => {
+            watcher();
+        }, 1000);
+
+        return () => clearInterval(interval);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [gameAddress]);
+
+    return (
+        <div >
+            {timeoutData && (
+                <div>
+                    {!p2Played ? (
+                        <>
+                            <div className="font-mono text-xl">
+                                {/* Time Remaining: {timer > 0 ? formatTime(timer) : "Time's up"} */}
+                                Timeout to call at: {timer}
+                            </div>
+                            <div>
+                                <Button
+                                    lable={'Timeout'}
+                                    onClick={callTimeout}
+                                />
+                            </div>
+                        </>
+                    ) : (
+                        <div className="text-green-600 font-bold text-xl">
+                            Player 2 has played their move!
+
+                        </div>
+                    )}
+                </div>
+            )}
+
+        </div>
+    )
+}
+
+export function J2Timeout({ gameAddress, played }: { gameAddress: string, played: boolean }) {
+
+    const [timer, setTimer] = useState<string>();
+    // const [p2Played, setP2Played] = useState(false);
+
+    function timeout() {
+        try {
+            if (played) {
+                const timeoutdata = new Date(Date.now() + 5 * 60 * 1000).toLocaleTimeString("en-GB", { hour12: false });
+                setTimer(timeoutdata);
+            }
+        } catch (e) { console.log(e) }
+    }
+
+    async function callTimeout() {
+        try {
+            const signer = await getSigner();
+            const contract = new ethers.Contract(gameAddress, RPSArtificate.abi, signer);
+            const j2Timeout = await contract.j1Timeout();
+            await j2Timeout.wait();
+            alert(` executed successfully!`);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    useEffect(() => { timeout() }, [played])
+    return (
+        <div >
+            {/* {timer && (
+                <div>
+                    {!p2Played ? (
+                        <div className="font-mono text-xl">
+                            Time Remaining: {timer > 0 ? formatTime(timer) : "Time's up"}
+                            Timeout to call at: {timer}
+                        </div>
+                    ) : (
+                        <div className="text-green-600 font-bold text-xl">
+                            Player 2 has played their move!
+
+                        </div>
+                    )}
+                </div>
+            )} */}
+
+            {played && <>
+                <div>
+                    {timer}
+                </div>
+                <div>
+                    <Button
+                        lable={'Timeout'}
+                        onClick={callTimeout}
+                    />
+                </div>
+            </>}
+        </div>
+    )
+}
+
