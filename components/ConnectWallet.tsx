5dd8d7d0fd8c04c57b2598448a0661f77045c3c1
om2137 <omraut961@gmail.com>
2025-11-21 02:13:27 +0530

    connect wallet component

--- components/ConnectWallet.tsx
+++ components/ConnectWallet.tsx
@@ -4,7 +4,7 @@ import { getProvider, getSigner } from "@/utils/connect";
 import { ethers } from "ethers";
 import { Dispatch, SetStateAction, useState } from "react";
 
-export default function Wallet({address,setAddress}:{address:string;setAddress:Dispatch<SetStateAction<string>>}) {
+export default function Wallet({ address, setAddress }: { address: string; setAddress: Dispatch<SetStateAction<string>> }) {
 
     const [loading, setLoading] = useState(false);
     const [balance, setBalance] = useState<string>("0.0");
@@ -34,23 +34,29 @@ export default function Wallet({address,setAddress}:{address:string;setAddress:D
     // useEffect(() => { loadWallet() }, [])
 
     return (
-        <div className="flex flex-col  gap-4 items-center justify-start py-10">
-            <div className="text-2xl capitalize">
-                kleros RPSLS
-            </div>
-            <div>
-                <Button lable={loading ? `Connecting...` : `Connect`} onClick={loadWallet} />
-            </div>
-            <div className="flex flex-col">
+        <div className="flex flex-col bg-sky-100 rounded-xl m-10 p-6 gap-4 items-center justify-start py-10">
+            {network ? <div className="flex flex-col gap-2">
+                <h1 className="text-xl font-semibold">Connected User Information</h1>
                 <div>
-                    {address ? `connected address: ${address}` : 'no address available'}
-                </div>
-                <div>
-                    {balance ? `${balance} ETH` : `no balance available`}
-                </div>
-                <div>
+                    <span className="font-semibold">Connected Network: </span>
                     {network ? `${network}` : `no network available`}
                 </div>
+                <div>
+                    <span className="font-semibold">User Address: </span>
+                    {address ? `${address}` : 'no address available'}
+                </div>
+                <div>
+                    <span className="font-semibold">User Balance: </span>
+                    {balance ? `${balance} ETH` : `no balance available`}
+                </div>
+
+            </div> :
+                <div className="font-bold text-xl">
+                    Connect Wallet
+                </div>
+            }
+            <div>
+                <Button lable={loading ? `Connecting...` : `Connect`} onClick={loadWallet} />
             </div>
         </div>
     );
